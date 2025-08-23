75c44290fa96a4f97ce2735eec03d6f6275f6051
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-08-23 15:21:26 -0300

    creates the createUser controller

--- backend/src/controllers/createUser.ts
+++ backend/src/controllers/createUser.ts
@@ -1,9 +1,35 @@
 // Modules
 import { Request, Response } from "express"
+import prisma from "../config/prisma"
+
+interface UserDTO {
+    user: string,
+    password: string
+}
 
 const createUser = async(req: Request, res: Response) => {
     try {
-        
+        const { user, password }: UserDTO = req.body
+
+        if (!user || !password) {
+            res.status(400).json("Dados invalidos")
+            return
+        }
+
+        const checkUser = await prisma.user.findUnique({ where: { user } })
+        if (checkUser) {
+            res.status(400).json({ msg: "Usuario já existe" })
+            return
+        }
+
+        const newUser = await prisma.user.create({
+            data: {
+                user,
+                password,
+            }
+        })
+
+        res.status(201).json({ msg: "Usuario criado com sucesso", newUser })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente" })
     }
