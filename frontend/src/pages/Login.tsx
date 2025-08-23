679190c60b2fd49cb1a717f04da974449772c692
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-08-23 02:05:32 -0300

    creates the login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,16 @@
-
-
 const Login = () => {
     return (
-        <div>
-            <div>
-                <label>
-                    <p>Usuario: </p>
-                    <input type="text" />
-                </label>
-            </div>
+        <div className="flex flex-col h-screen items-center justify-center text-white">
+            <label className="mb-5">
+                <p>Usuario: </p>
+                <input type="text" className="border-2 border-black rounded-md indent-1 mt-2"   />
+            </label>
+            <label>
+                <p>Senha: </p>
+                <input type="password" className="border-2 border-black rounded-md indent-1 mt-2" />
+            </label>
+
+            <button className="mt-5 border-2 border-white rounded-md p-1 hover:bg-white hover:text-black">Logar</button>
         </div>
     )
 }
