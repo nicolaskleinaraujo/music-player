48c59729804931d99007247a941e1b09eb92a9e0
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-08-23 23:22:54 -0300

    creates the createLogin controller

---
+++ backend/src/controllers/createLogin.ts
@@ -0,0 +1,34 @@
+// Modules
+import { Request, Response } from "express"
+import bcrypt from "bcryptjs"
+import prisma from "../config/prisma"
+
+interface UserDTO {
+    user: string,
+    password: string,
+}
+
+const createLogin = async (req: Request, res: Response) => {
+    try {
+        const { user, password }: UserDTO = req.body
+
+        const searchUser = await prisma.user.findUnique({ where: { user } })
+        if (!searchUser) {
+            res.status(400).json({ msg: "Usuario ou senha incorretos" })
+            return
+        }
+
+        // Checks hash with password
+        const checkPassword: boolean = await bcrypt.compare(password, searchUser.password)
+        if (!checkPassword) {
+            res.status(400).json({ msg: "Usuario ou senha incorretos" })
+            return
+        }
+
+        res.status(200).json({ msg: "Logado com sucesso", searchUser })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
+    }
+}
+
+export default createLogin
