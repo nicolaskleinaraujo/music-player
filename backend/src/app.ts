eb391811e0d06c46ab667bdb4e7bcf2aa49041ba
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-08-23 23:28:01 -0300

    adds the createLogin to the router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import cors from "cors"
 
 // Controllers
 import createUser from "./controllers/createUser"
+import createLogin from "./controllers/createLogin"
 
 const app: Application = express()
 const port: number = Number(process.env.PORT) || 3000
@@ -17,6 +18,7 @@ app.use(
 )
 
 app.post("/user", (req: Request, res: Response) => createUser(req, res))
+app.post("/user/login", (req: Request, res: Response) => createLogin(req, res))
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
