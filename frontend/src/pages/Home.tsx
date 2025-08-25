eb176202a2718128e7f3dd7698add6b6b1afaf0d
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-08-25 17:31:59 -0300

    adds audioPlayer component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 // Modules
-import dbFetch from "../utils/axios"
+import AudioPlayer from "react-h5-audio-player"
+import "react-h5-audio-player/lib/styles.css"
 
 const Home = () => {
     return (
-        <div className="flex flex-col justify-center items-center text-white gap-7 text-center">
+        <div className="flex flex-col justify-center items-center text-white gap-7 text-center pt-8">
             <div>
                 <h2>Playlist Name 1</h2>
                 <ul>
@@ -20,6 +21,13 @@ const Home = () => {
                     <li>Music 3</li>
                 </ul>
             </div>
+
+            <button className="border-2 border-white rounded-md p-2">Nova Playlist</button>
+
+            <AudioPlayer
+                src="/test-audios/music_1.mp3"
+                onPlay={() => console.log("Playing...")}
+            />
         </div>
     )
 }
