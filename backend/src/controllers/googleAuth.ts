09e74737f93bcc601fa23710ee1b7d2a7bef9ab4
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-09-03 21:01:20 -0300

    creates the googleAuth controller

--- backend/src/controllers/googleAuth.ts
+++ backend/src/controllers/googleAuth.ts
@@ -1,17 +1,22 @@
 // Modules
 import { Request, Response } from "express"
 import prisma from "../config/prisma"
-import jwt from "jsonwebtoken"
 import { OAuth2Client } from "google-auth-library"
 
 const client = new OAuth2Client(String(process.env.GOOGLE_CLIENT_ID))
 
 const googleAuth = async(req: Request, res: Response) => {
     try {
-        const { credential } = req.body
+        const { credential, userId } = req.body
 
-        if (!credential) {
+        if (!credential || userId) {
             res.status(400).json({ msg: "Informações insuficientes" })
+            return
+        }
+
+        const userExists = await prisma.user.findUnique({ where: { id: userId } })
+        if (!userExists) {
+            res.status(404).json({ msg: "Usuario não encontrado" })
             return
         }
 
@@ -26,7 +31,12 @@ const googleAuth = async(req: Request, res: Response) => {
             return
         }
 
-        
+        const user = await prisma.user.update({
+            where: { id: userId },
+            data: { email: payload.email },
+        })
+
+        res.status(200).json({ msg: "Logado com sucesso", user })
     } catch (error) {
         res.status(500).json({ msg: "Erro interno, tente novamente", error })
     }
