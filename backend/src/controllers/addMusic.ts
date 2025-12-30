c4856a3f77be8740cb3b52a1b82b288a836df048
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-12-29 23:51:43 -0300

    remade all the addMusic logic

--- backend/src/controllers/addMusic.ts
+++ backend/src/controllers/addMusic.ts
@@ -1,19 +1,16 @@
 // Modules
 import { Request, Response } from "express"
 import prisma from "../config/prisma"
-import path from "path"
-import fs from "fs"
-import YTDlpWrap from "yt-dlp-wrap"
-import generateCookies from "../config/generateCookies"
+import path from "node:path"
+import fs from "node:fs"
+import https from "node:https"
+import axios from "axios"
 
 interface MusicDTO {
     url: string,
     playlistId: number,
     userId: number,
 }
-
-const ytDlpPath = path.resolve(__dirname, String(process.env.YT_DLP_URL))
-const ytDlp = new YTDlpWrap(ytDlpPath)
 
 const addMusic = async(req: Request, res: Response) => {
     try {
@@ -24,34 +21,55 @@ const addMusic = async(req: Request, res: Response) => {
             return
         }
 
+        const authGen = await axios.get(String(process.env.AUTH_GEN_URL), {
+            headers: {
+                "Origin": "https://frame.y2meta-uk.com",
+                "User-Agent": "PostmanRuntime/7.49.1"
+            },
+        })
+
+        const urlGen = await axios.post(String(process.env.URL_GEN), {
+            "link": url,
+            "format": "mp3",
+            "audioBitrate": "320",
+            "videoQuality": "720",
+            "filenameStyle": "pretty",
+            "vCodec": "h264"
+        }, {
+            headers: {
+                "Key": authGen.data.key,
+                "Origin": "https://frame.y2meta-uk.com",
+                "User-Agent": "PostmanRuntime/7.49.1",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept": "*/*",
+                "Content-Type": "application/json",
+                "Connection": "keep-alive"
+            }
+        })
+
         const musicDir = path.resolve(__dirname, "../../musics")
         if (!fs.existsSync(musicDir)) fs.mkdirSync(musicDir, { recursive: true })
 
-        const musicPath = path.join(musicDir, "%(title)s_%(id)s.%(ext)s")
+        const musicFile = path.join(musicDir, "musicTest.mp3")
 
-        const cookiesTxtPath = await generateCookies(userId)
-        if (!cookiesTxtPath) {
-            res.status(400).json({ msg: "Erro ao gerar cookies" })
-            return
-        }
+        https.get(urlGen.data.url, (response) => {
+            if (response.statusCode !== 200) {
+                res.status(500).json({ msg: "Erro interno, tente novamente" })
+                return
+            }
 
-        const infoJson = await ytDlp.execPromise([
-            url,
-            "-x",
-            "--cookies", cookiesTxtPath,
-            "--audio-format", "mp3",
-            "-o", musicPath,
-            "--print-json"
-        ])
+            const fileStream = fs.createWriteStream(musicFile)
+            response.pipe(fileStream)
 
-        const parsedMusicInfo = await JSON.parse(infoJson)
+            fileStream.on("finish", () => fileStream.close())
+        })
 
         const music = await prisma.music.create({
             data: {
-                title: parsedMusicInfo.title,
-                channel: parsedMusicInfo.uploader,
+                title: urlGen.data.filename,
+                channel: "placeholder",
                 url,
-                filePath: `${parsedMusicInfo.title}_${parsedMusicInfo.id}.mp3`,
+                filePath: musicFile,
                 Playlist: { connect: { id: playlistId } }
             }
         })
