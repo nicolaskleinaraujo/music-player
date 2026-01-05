ba3bea699dc45880b106ad654e09c20ef86b5de0
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2026-01-04 21:06:08 -0300

    adds form to the login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,6 +3,7 @@ import dbFetch from "../utils/axios"
 import { useContext, useState } from "react"
 import { UserContext } from "../context/UserContext"
 import { useNavigate } from "react-router-dom"
+import type { FormEvent } from "react"
 
 const Login = () => {
     const navigate = useNavigate()
@@ -12,7 +13,9 @@ const Login = () => {
 
     const { setUserId, setPlaylists } = useContext(UserContext)
 
-    const handleLogin = async() => {
+    const handleLogin = async(e: FormEvent) => {
+        e.preventDefault()
+
         try {
             const res = await dbFetch.post("/user/login", {
                 user,
@@ -31,25 +34,28 @@ const Login = () => {
     }
 
     return (
-        <div className="flex flex-col h-screen items-center justify-center text-white">
-            <label className="mb-5">
-                <p>Usuario: </p>
-                <input 
-                    type="text"
-                    className="border-2 border-black rounded-md indent-1 mt-2" 
-                    onChange={(e) => setUser(e.target.value)}
-                />
-            </label>
-            <label>
-                <p>Senha: </p>
-                <input 
-                    type="password"
-                    className="border-2 border-black rounded-md indent-1 mt-2" 
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </label>
+        <div>
+            <form onSubmit={(e) => handleLogin(e)} className="flex flex-col h-screen items-center justify-center text-white">
+                <label className="mb-5">
+                    <p>Usuario: </p>
+                    <input 
+                        type="text"
+                        className="border-2 border-black rounded-md indent-1 mt-2" 
+                        onChange={(e) => setUser(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <p>Senha: </p>
+                    <input 
+                        type="password"
+                        className="border-2 border-black rounded-md indent-1 mt-2" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
 
-            <button className="mt-5 border-2 border-white rounded-md p-1 hover:bg-white hover:text-black" onClick={() => handleLogin()}>Logar</button>
+            <input className="mt-5 border-2 border-white rounded-md p-1 hover:bg-white hover:text-black" type="submit" value="Logar" />
+            </form>
+
         </div>
     )
 }
