3ea5360878b6b6bcc685932034caf1a5170583be
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2026-01-07 19:27:49 -0300

    adds form to the searchBox component

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,9 +1,11 @@
 // Modules
 import dbFetch from "../utils/axios"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
 
 const SearchBox = () => {
+    const navigate = useNavigate()
+
     const [query, setQuery] = useState("")
     const [suggestedMusics, setSuggestedMusics] = useState([])
     const [isFocused, setIsFocused] = useState(false)
@@ -21,21 +23,23 @@ const SearchBox = () => {
     return (
         <div className="w-full max-w-md mx-auto text-white">
             <div className="relative">
-                <input 
-                    onChange={(e) => {
-                        setQuery(e.target.value)
-                        getMusicSuggestion(e.target.value)
-                    }}
-                    value={query}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setTimeout(() => setIsFocused(false), 200)}
-                    placeholder="Pesquisar musicas..."
-                    className="w-full bg-[#121212] text-white
-                        px-4 py-2 rounded-md
-                        border border-gray-700
-                        focus:outline-none focus:border-[#1db954]
-                        placeholder-gray-400"
-                />
+                <form onSubmit={(e) => {e.preventDefault(), navigate(`/search?q=${query}`)}}>
+                    <input 
+                        onChange={(e) => {
+                            setQuery(e.target.value)
+                            getMusicSuggestion(e.target.value)
+                        }}
+                        value={query}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+                        placeholder="Pesquisar musicas..."
+                        className="w-full bg-[#121212] text-white
+                            px-4 py-2 rounded-md
+                            border border-gray-700
+                            focus:outline-none focus:border-[#1db954]
+                            placeholder-gray-400"
+                    />
+                </form>
             </div>
             {isFocused && query !== "" && suggestedMusics.length > 0 && (
                 <ul className="mt-2 bg-[#181818] rounded-md overflow-hidden divide-y divide-gray-700">
